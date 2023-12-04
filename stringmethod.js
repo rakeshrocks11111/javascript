d257e98f2f6c6b5b7948c1cb08735d47f9367952
rakeshrocks11111 <rakeshrocks11111@gmail.com>
2023-12-04 13:17:42 +0530

    added string file

---
+++ stringmethod.js
@@ -0,0 +1,16 @@
+// let mobileNo = "9573303557"
+// const slicePart=mobileNo.slice(-3)
+// console.log(slicePart)
+// const finalOutput=slicePart.padStart(10,"*")
+// console.log(finalOutput)
+
+
+// let mobileNo = "9573303557"
+// const slicePart=mobileNo.slice(-3)
+// console.log(slicePart)
+// const finalOutput=slicePart.padStart(10,"*")
+// console.log(finalOutput)
+ 
+let mobileNo="9573303557"
+const slicePart = "957".padEnd(10,"*")
+console.log(slicePart)
