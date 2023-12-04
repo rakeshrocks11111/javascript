9168090fcc76556e226747c608001a2be56729e1
rakeshrocks11111 <rakeshrocks11111@gmail.com>
2023-12-04 12:51:16 +0530

    added new file

---
+++ ifElse.js
@@ -0,0 +1,54 @@
+// let text1 = 'rakesh father name is "raju"'
+// console.log(text1.length)
+
+// const name = "rakesh"
+// console.log(name[0])
+// console.log(name.slice(-5))
+
+// const question = "what is your name"
+// console.log(question)
+
+// let text = 'he is "excellent" in his work'
+// // console.log(text)
+// if (false){
+// console.log("movie")
+// }else{
+//     console.log("working day")
+// 
+
+// const time = 24
+
+// if (time<12){
+//     console.log("good morning")
+// }else if(time<18){
+//     console.log("good afternoon")
+// }else if(time>20){
+//     console.log("good day")
+// }
+// else{
+//     console.log("good evening")
+
+// let text1 = "Hello";
+// let text2 = "world";
+// let text3 = text1.concat("",text2);
+// // console.log(text3)
+
+// let text1 = "my name is rakesh"
+// let text2 = "Iam a working proffesinal"
+// city = "hyderabad"
+// age = 24
+// religon = "indian"
+// console.log(`${text1} ${text2} my father name is raju age is ${age} and  iam lives in${city} my religon is ${religon}`)
+//Helloworld
+let text1 = "This is an electricity office"
+let text2 = "here there are many employes"
+city = "hyderabad"
+area = "irrrum manzil"
+country = "india"
+console.log(`${text1} which is located in Telangana ${text2} this office provide electrity to the city of hyderabad ${city} ${area} ${country}`)
+// let fname="rakesh"
+// let lname="rocky"
+// age = 24
+// city = "hyderabad"
+// // console.log(`fname+ " " + lname + "is a good boy. and his age is " + age + " and he lives in " + city`)
+// console.log(`${fname} ${lname} is a good boy. his age is ${age} and he lives in ${city}`)
