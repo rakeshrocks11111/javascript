5cab4a3a6dabd7b0ab1ad841e22ffc3e7ae6a5db
rakeshrocks11111 <rakeshrocks11111@gmail.com>
2023-12-04 13:10:43 +0530

    added basic file

---
+++ basic1.js
@@ -0,0 +1,23 @@
+// console.log('rakesh');
+// // to add two numbers
+// let sum1 = (a, b) => {
+//     console.log(a + b);
+// };
+
+
+// sum1(15, 6);
+
+// // // sub( 15, 6)
+
+// let quantity = 10;
+
+// console.log('total chocolates rakesh have',quantity);
+
+
+console.log('Mahesh');
+let mad1 = (x, y) => {
+    console.log(x - y);
+};
+// mad1(100,40);
+// let quantity = 100;
+// console.log('total chocolates mahesh have',quantity);
